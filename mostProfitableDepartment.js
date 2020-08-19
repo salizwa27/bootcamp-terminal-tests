c5f66e925eceda8dcb76e749df51c4584a782dad
salizw27 <salizwamayekiso27@gmail.com>
2020-08-19 16:30:44 +0200

    finished with bootcamp-terminal tests

--- mostProfitableDepartment.js
+++ mostProfitableDepartment.js
@@ -1,30 +1,27 @@
 module.exports = function mostProfitableDepartment(department1){
-    var map = {};
-    var depName = '';
-
-     for (var i = 0; i < department1.length; i++){
-
-       var myDepartment = department1[i].department
-       
-        if(map[myDepartment] === undefined){
-           map[myDepartment] = 0;
-        }
-            map[myDepartment] += department1[i].sales
-     }
-    
-    
-      var greaterDep = "";
-      var greaterSales = 0;
-    
-      for( i in map){
-         if(map[i] > greaterSales){
-           greaterDep = i
-           greaterSales = map[i]
-    
-         }
-    
+                                               
+  var map = {};
+  var depName = '';
+   for (var i = 0; i < department1.length; i++){
+     var myDepartment = department1[i].department
+      if(map[myDepartment] === undefined){
+         map[myDepartment] = 0;
       }
-      
-      return greaterDep
-
-    }
+          map[myDepartment] += department1[i].sales;
+   }
+  
+  
+    var greaterDep = "";
+    var greaterSales = 0;
+  //console.log(map);
+  
+    for( i in map){
+       if(map[i] > greaterSales){
+         greaterDep = i
+         greaterSales = map[i]
+  
+       }
+  
+       
+    }return greaterDep
+  }
--- test/mostProfitableDepartment.test.js
+++ test/mostProfitableDepartment.test.js
@@ -5,7 +5,7 @@ let mostProfitableDepartment = require("../mostProfitableDepartment")
     describe("the mostProfitableDepartment function", function() {
       it("should give out the most profitable department", function() {
 
-        [{"department":"hardware","sales":4500,"day":"Monday"},
+       var list = [{"department":"hardware","sales":4500,"day":"Monday"},
         {"department":"outdoor","sales":1500,"day":"Monday"},
         {"department":"carpentry","sales":5500,"day":"Monday"},
         {"department":"hardware","sales":7500,"day":"Tuesday"},
@@ -21,6 +21,49 @@ let mostProfitableDepartment = require("../mostProfitableDepartment")
         {"department":"outdoor","sales":12006,"day":"Friday"},
         {"department":"carpentry","sales":16109,"day":"Friday"}]
 
-        assert.equal( mostProfitableDepartment('greaterDep'))
+        assert.deepEqual("outdoor", mostProfitableDepartment(list))
       })
+
+      it("should give out the most profitable department", function() {
+
+      var list = [{"department":"hardware","sales":4500,"day":"Monday"},
+      {"department":"outdoor","sales":1500,"day":"Monday"},
+      {"department":"carpentry","sales":5500,"day":"Monday"},
+      {"department":"hardware","sales":7500,"day":"Tuesday"},
+      {"department":"outdoor","sales":2505,"day":"Tuesday"},
+      {"department":"carpentry","sales":11540,"day":"Tuesday"},
+      {"department":"hardware","sales":1500,"day":"Wednesday"},
+      {"department":"outdoor","sales":8507,"day":"Wednesday"},
+      {"department":"carpentry","sales":18009,"day":"Wednesday"},
+      {"department":"hardware","sales":12000,"day":"Thursday"},
+      {"department":"outdoor","sales":18007,"day":"Thursday"},
+      {"department":"carpentry","sales":16109,"day":"Thursday"},
+      {"department":"hardware","sales":7005,"day":"Friday"},
+      {"department":"outdoor","sales":12006,"day":"Friday"},
+      {"department":"carpentry","sales":16109,"day":"Friday"}]
+
+      assert.deepEqual("carpentry", mostProfitableDepartment(list))
+    })
+
+    it("should give out the most profitable department", function() {
+
+      var list = [{"department":"hardware","sales":4500,"day":"Monday"},
+       {"department":"outdoor","sales":1500,"day":"Monday"},
+       {"department":"carpentry","sales":5500,"day":"Monday"},
+       {"department":"hardware","sales":17500,"day":"Tuesday"},
+       {"department":"outdoor","sales":2505,"day":"Tuesday"},
+       {"department":"carpentry","sales":1540,"day":"Tuesday"},
+       {"department":"hardware","sales":21500,"day":"Wednesday"},
+       {"department":"outdoor","sales":8507,"day":"Wednesday"},
+       {"department":"carpentry","sales":8009,"day":"Wednesday"},
+       {"department":"hardware","sales":12000,"day":"Thursday"},
+       {"department":"outdoor","sales":18007,"day":"Thursday"},
+       {"department":"carpentry","sales":6109,"day":"Thursday"},
+       {"department":"hardware","sales":17005,"day":"Friday"},
+       {"department":"outdoor","sales":12006,"day":"Friday"},
+       {"department":"carpentry","sales":16109,"day":"Friday"}]
+
+       assert.deepEqual("hardware", mostProfitableDepartment(list))
+     })
+  
     })
